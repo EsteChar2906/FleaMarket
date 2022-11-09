a658d09c4c2e496f69efdf32b3a65517a689b363
JulianLIMe <linero.julian.andres@gmail.com>
2022-11-09 14:19:57 -0500

    añadiendo try catch en ruta post, sin comentarios

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -4,13 +4,6 @@ import Products from "../models/products.js";
 const router = express();
 
 router.post("/product", async (req, res) => {
-    /* const newProduct =  Products(req.body);
-    newProduct.save()
-    .then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({ message: err })
-    }) */
     try {
         const newProduct = Products(req.body);
         await newProduct.save()
