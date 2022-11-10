c2c6aa7b0df40d7226f89cdcfd5c22484b21115e
harveyzambrano <backtrack815@gmail.com>
2022-11-10 09:19:19 -0500

    Editando Detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,22 +1,59 @@
-import React from "react";
-import { useEffect } from "react";
+import React from 'react'
+import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+//import { getDetail} from '../../actions'
+import data, {product1} from '../../testData.js';
 
+export  function Detail() {
 
-export default function Detail(){
-   /*  const dispatch = useDispatch()
+ const getDetail= () => {
+    return product1
+  }
+    const p = getDetail()
+    console.log(p)
+  
+ let params = useParams()
+ console.log(params.id)
+ 
+  /* const dispatch = useDispatch()
     let {id} = useParams()
     const producto = useSelector((state) => state.detail)
-    console.log(producto) */
 
-  /*   useEffect(()=> {
+     useEffect(()=> {
         dispatch(getDetail(id))
     },[dispatch]) */
 
-    return(
-        <div>
-            <img className="img-Detail" src={producto.image} />
-        </div>
-    )
+
+
+
+  return (
+    <> 
+    Editando Detalle
+      <div>        
+        <img src={p.image} width="250px" height="250px"/>
+      </div>
+      <div>
+        <a>Name: {product1.title}</a>
+      </div>
+      <div>
+        <a>Category: {product1.category}</a>
+      </div>      
+      <div>
+        <a>{product1.stock}</a>
+      </div>
+      <div>
+        <a>Description: {product1.description}</a>
+      </div>
+      <div>
+        <a>Price: {product1.price}</a>
+      </div>
+      <div>
+        <a>{product1.condition}</a>
+      </div>
+      <div>
+        <a>Rating: {product1.rating.rate}</a>
+      </div>
+    </>
+  )
 }
