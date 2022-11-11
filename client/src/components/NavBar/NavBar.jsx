4cb9e6970255301e8a21340e2e5c5196de11a090
MilagrosVerde <milagrosverde1234@gmail.com>
2022-11-11 13:47:54 -0300

    Estilos todo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,25 @@
 import { Link } from 'react-router-dom';
 import {SearchBar} from '../SearchBar/SearchBar';
+import "./NavBar.css"
+import logo from "../../images/logouno.png"
+import carrito from "../../images/carrito.png"
 
 // Dummy component
 // const SearchBar = () => <input type="text" defaultValue="Buscar" />
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
+    <nav className='navbar'>
+      <ul className='navbardos'>
         <li>
           <Link to="/">
-            Logo
+            <img className='logonav' src={logo} alt="" />
           </Link>
         </li>
         <li>
           <SearchBar />
         </li>
-        <li>CARRITO</li>
+        <li><img className='carrito' src={carrito} alt="" /></li>
       </ul>
     </nav>
   )
