5fc0a52a6bb28a3b3c42c154d2706722602431f3
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-11-12 19:45:07 -0500

    Encerrando al componente App en el Auth0Provider

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,17 @@ import App from "./App";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <Auth0Provider
+    domain="dev-867ewf0h2lv0q1d7.us.auth0.com"
+    clientId="AgP0AfHrsyn1N2UqYRs7aOcXcbviE4yK"
+    redirectUri={window.location.origin}
+    > 
+      <App />
+    </Auth0Provider>
   </Provider>
 );
