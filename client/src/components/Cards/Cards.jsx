5698e8ecee426bb1ff93dd9b3741c9b81486cece
MilagrosVerde <milagrosverde1234@gmail.com>
2022-11-16 12:45:01 -0300

    paginado

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,16 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
 import Card from "./Card.jsx";
 import "./Cards.css";
+import Pagination from "../Pagination/Pagination.jsx"
 
 export default () => {
     const products = useSelector(state => state.products);
     const filtered = useSelector(state => state.filtered);
+    const dispatch = useDispatch() 
 
-    const productsToShow = filtered.length === 0 ? products : filtered;
+    //useEffect(() => {if (estado.length===0){ products }}, [])
+
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage, setProductsPerPage] = useState(8);
+
+    let lastProductIndex = currentPage * productsPerPage;
+    let firstProductIndex = lastProductIndex - productsPerPage;
+    let currentProducts = products.slice(firstProductIndex, lastProductIndex);
+
+    const productsToShow = filtered.length === 0 ? currentProducts : filtered;
+
+    const paginado = (pageNum) => {
+        setCurrentPage(pageNum)
+    }
 
     return (
-        <div className="contenedorCards">
+        <div>
+            <Pagination
+                currentPage = {currentPage}
+                productsPerPage = {productsPerPage}
+                products = {products.length}
+                paginado = {paginado}
+            />
+            <div className="contenedorCards">
+            
                 {   
                     productsToShow.map((e, i) => {
                         return (<Card
@@ -23,6 +48,11 @@ export default () => {
                     }) 
                     
                 }
+            </div>
+
+          
+
         </div>
+        
     )
 }
