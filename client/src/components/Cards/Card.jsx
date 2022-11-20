2f9e03b8d5f44e0dd16f68dd5b4f228cde05815c
harveyzambrano <backtrack815@gmail.com>
2022-11-20 11:57:01 -0500

    reorganizando front

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,26 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Card.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Card.css'
 
 export default (props) => {
-    return (
-        <div className="block__card">
-
-      
-        <Link to={`/detail/${props.id}`}>
-          <img className="block__card-img" src={props.image} alt="not found" />
-        </Link>
-        <div className="block__details">
-          <div className="block__name-precio">
-            <h3 className="block__card-name">{props.title}</h3>
-            <div className="block__card-precio">${props.price}</div>
-          </div>
-          <div className="block__description">
-            <div className="block__card-agregar">IconoCAr</div>
-          </div>
+  return (
+    <div className="block__card">
+      <Link to={`/detail/${props.id}`}>
+        <img className="block__card-img" src={props.image} alt="not found" />
+      </Link>
+      <div className="block__details">
+        <div className="block__name-precio">
+          <h3 className="block__card-name">{props.title}</h3>
+          <div className="block__card-precio">${props.price}</div>
         </div>
-  
-  
+        <div className="block__description">
+          <div className="block__card-agregar">IconoCAr</div>
+        </div>
       </div>
-    )
+    </div>
+  )
 }
