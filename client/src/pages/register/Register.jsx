7705b93c29e0b44001c18f1489c19a4949202f6c
Jose David Lara <jose-dld@hotmail.com>
2022-11-21 16:00:06 -0500

    arreglo del registro

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -21,13 +21,14 @@ const Register = () => {
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
+  console.log(data);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(e.target.value)
     try {
       const url = "http://localhost:3001/api/register";
-      const formPost = await axios.post(url, data);
-      console.log(formPost)
+       await axios.post(url, data);
       history.push("/login");
       setData({
         firstname: "",
@@ -47,132 +48,123 @@ const Register = () => {
 
   return (
     <div className={styles.register_containercero}>
-        <div className={styles.register_container}>
+      <div className={styles.register_container}>
+        <div className={styles.register_welcome}>
+          <Link
+            to="/login"
+            style={{ textDecoration: "none" }}
+            className={styles.register_welcome_textuno}
+          >
+            <h2 className={styles.register_welcome_textuno}>Welcome Back</h2>
+          </Link>
 
-
-
-          <div className={styles.register_welcome} >
-            <Link to='/login' style={{textDecoration: "none" } } className={styles.register_welcome_textuno}>
-              <h2 className={styles.register_welcome_textuno} >Welcome Back</h2>
+          <div className={styles.register_welcome_texttres}>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <p>Sing in</p>
             </Link>
-    
-            <div className={styles.register_welcome_texttres}>
-              <Link to='/login' style={{textDecoration: "none" } }>
-                <p >Sing in</p>
-              </Link>
-              <Link to='/' style={{textDecoration: "none" } } >
-                <p >Back to Home</p>
-              </Link>
-            </div>
-            
-    
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <p>Back to Home</p>
+            </Link>
           </div>
-    
-    
-          <div className={styles.form_container}>
-            <h1 className={styles.form_container_mjs}>Create Account</h1>
-            <form className={styles.form_container_formulario} onSubmit={handleSubmit}>
-              
-                <input 
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={handleChange}
-                value={data.email}
-                required
-                className={styles.input}
-              />
-              
-              <input
-                type="text"
-                placeholder="First Name"
-                name="firstname"
-                onChange={handleChange}
-                value={data.firstname}
-                required
-                className={styles.input}
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                name="lastname"
-                onChange={handleChange}
-                value={data.lastname}
-                required
-                className={styles.input}
-              />
-              <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                onChange={handleChange}
-                value={data.username}
-                required
-                className={styles.input}
-              />
-  
-              <input
-                type="country"
-                placeholder="Country"
-                name="country"
-                onChange={handleChange}
-                value={data.country}
-                required
-                className={styles.input}
-              />
-              <input
-                type="city"
-                placeholder="City"
-                name="city"
-                onChange={handleChange}
-                value={data.city}
-                required
-                className={styles.input}
-              />
-              <input
-                type="addres"
-                placeholder="Addres"
-                name="addres"
-                onChange={handleChange}
-                value={data.addres}
-                required
-                className={styles.input}
-              />
-              <input
-                type="telephone"
-                placeholder="Telephone"
-                name="telephone"
-                onChange={handleChange}
-                value={data.telephone}
-                required
-                className={styles.input}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                onChange={handleChange}
-                value={data.password}
-                required
-                className={styles.input}
-              />
-  
-            </form>
-            <button type="submit" className={styles.green_btn}>
-                Registrarse
-            </button>
-
         </div>
 
+        <div className={styles.form_container}>
+          <h1 className={styles.form_container_mjs}>Create Account</h1>
+          {error&&error}
+          <form
+            className={styles.form_container_formulario}
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
 
-        {error&&error}
+            <input
+              type="text"
+              placeholder="First Name"
+              name="firstname"
+              onChange={handleChange}
+              value={data.firstname}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lastname"
+              onChange={handleChange}
+              value={data.lastname}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+              value={data.username}
+              required
+              className={styles.input}
+            />
 
+            <input
+              type="country"
+              placeholder="Country"
+              name="country"
+              onChange={handleChange}
+              value={data.country}
+              required
+              className={styles.input}
+            />
+            <input
+              type="city"
+              placeholder="City"
+              name="city"
+              onChange={handleChange}
+              value={data.city}
+              required
+              className={styles.input}
+            />
+            <input
+              type="addres"
+              placeholder="Addres"
+              name="addres"
+              onChange={handleChange}
+              value={data.addres}
+              required
+              className={styles.input}
+            />
+            <input
+              type="telephone"
+              placeholder="Telephone"
+              name="telephone"
+              onChange={handleChange}
+              value={data.telephone}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+          <button type='submit' className={styles.green_btn}>
+            Registrarse
+          </button>
+          </form>
+        </div>
 
-
-
-
-
-
+       
       </div>
     </div>
   );
