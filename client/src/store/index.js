bf2d123dfe8c72d085fad51250cecf9f3e97e9ab
Julian Andres <107429149+JulianLIMe@users.noreply.github.com>
2022-11-20 20:36:51 -0500

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
-import allReducers from "../reducers";
+import allReducers from "./reducers";
 
 const store = createStore(
   allReducers,
