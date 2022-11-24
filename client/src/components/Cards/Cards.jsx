021dbef036c4cd8c676f8ccfec9d8521a8ec2743
JulianLIMe <linero.julian.andres@gmail.com>
2022-11-23 19:36:46 -0500

    moficaciones en el carrito y el detalle

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux'
 import Card from './Card.jsx'
 import styles from './Cards.module.css'
-import { loadProducts } from '../../store/actions/index.js'
+import { loadProducts, loadProduct } from '../../store/actions/index.js'
 import { useEffect} from 'react'
 
 const Cards = () => {
   const dispatch = useDispatch()
   const products = useSelector((state) => state.products)
+
+  useEffect(() => { dispatch(loadProduct()) }, [])
 
   useEffect(() => {
     dispatch(loadProducts())
--- client/src/store/reducers/productReducer.js
+++ client/src/store/reducers/productReducer.js
@@ -2,7 +2,10 @@ function productReducer(state = {}, action) {
   if (action.type === "LOAD_PRODUCT") {
     return action.payload
   }
-
+  if (action.type === "void") {
+    return {}
+  }
+  
   return state;
 }
 
