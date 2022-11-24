e2a703ad4735ba345c1c171d2742287bc2ae3902
MilagrosVerde <milagrosverde1234@gmail.com>
2022-11-23 22:01:14 -0300

    modulecssdetail

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import './Detail.css';
 import { useSelector, useDispatch } from "react-redux";
 import {
   loadProduct,
   shopingCar,
 } from "../../store/actions/index.js";
-// import styles from "./Detail.module.css";
+import styles from "./Detail.module.css";
+import HeadPage from "../../components/HeadPage/HeadPage";
 
 export function Detail() {
   let { id } = useParams();
@@ -26,14 +26,24 @@ export function Detail() {
 
   return (
     <div>
-      <div>
-        <div>
-          <img />
-        </div>
-        <div>
-          <div>{p.title}</div>
+      <HeadPage />
+      <div className={styles.block__detail}>
+      <div className={styles.block__detail_contenedorcero}>
+      <div className={styles.block__detail_contenedor}>
+      
+      <div className={styles.block__detail_product}>
+      
+ 
+      <img className={styles.block__detail_productfoto}src={p.image} alt="" />
+         
+      </div>
+      <div className={styles.block__detail_info}>
+      <div className={styles.block__detail_titulo}>{p.title}</div>
+        
 
-          <div>
+          
+
+          <div className={styles.block__detail_estrellas}>
             <svg
               aria-hidden="true"
               class="w-5 h-5 text-yellow-400"
@@ -87,31 +97,196 @@ export function Detail() {
 
             {p.rating}
           </div>
-          <div>Precio: ${p.price}</div>
-
-          <div>
-            <div>Condition: {p.condition}</div>
-
-            <div>
-              {p.brand && <p>Marca: {p.brand}</p>}
-              {p.ram && <p>ram: {p.ram} GB</p>}
-              {p.processor && <p>procesador: {p.processor}</p>}
-              {p.battery && <p>uso continuo: {p.battery} horas</p>}
-              {p.bluetooth && <p>bluetooth</p>}
-            </div>
-            <div>
-              Description:
-              {p.description}
-            </div>
-
-            <div>Category: {p.category?.name}</div>
-          </div>
-
-          <button onClick={addToCar}>Add to my Car</button>
         </div>
+          
+          
+          
+        <div className={styles.block__detail_precio}>Precio: ${p.price}</div>
+        
+
+        <div className={styles.block__detail_caracteristicas}>
+          
+          
+          <div className={styles.block__detail_caracteristicasdatos}>
+          
+            Category:{p.category?.name}
+            
+            {p.brand && <p>Marca: {p.brand}</p>}
+            Condition:{p.condition}
+            {p.ram && <p>ram: {p.ram} GB</p>}
+            {p.processor && <p>procesador: {p.processor}</p>}
+            {p.battery && <p>uso continuo: {p.battery} horas</p>}
+            {p.bluetooth && <p>bluetooth</p>}
+            Description:{p.description}
+            
+          </div>
+          
+          
+          
+        </div>
+        <button className={styles.green_btn} onClick={addToCar}>Add to my Car</button>
+
+        <div>
+        
+
+       
+
+        
       </div>
+    </div>
+        
+      </div>
+
+    </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+/*import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  loadProduct,
+  shopingCar,
+} from "../../store/actions/index.js";
+import styles from "./Detail.module.css";
+import HeadPage from "../../components/HeadPage/HeadPage";
+
+export function Detail() {
+  let { id } = useParams();
+
+  const dispatch = useDispatch();
+  const p = useSelector((state) => state.product);
+
+
+  useEffect(() => {
+    dispatch(loadProduct(id));
+  }, []);
+
+  function addToCar() {
+    console.log("In SHOPPING CAR");
+    dispatch(shopingCar("ADD", p));
+  }
+
+  return (
+    <div>
+      <HeadPage />
+      <div className={styles.block__detail}>
+      <div className={styles.block__detail_contenedorcero}>
+      <div className={styles.block__detail_contenedor}>
+      
+      <div className={styles.block__detail_product}>
+ 
+      <img className={styles.block__detail_productfoto}src={p.image} alt="" />
+         
+      </div>
+      <div className={styles.block__detail_info}>
+        <div className={styles.block__detail_infouno}>
+
+          <div className={styles.block__detail_titulo}>{p.title}</div>
+
+          <div className={styles.block__detail_estrellas}>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>First star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Second star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Third star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Fourth star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-300 dark:text-gray-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Fifth star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+
+            {p.rating}
+          </div>
+        </div>
+          
+          
+          
+        
+        
+
+        <div className={styles.block__detail_caracteristicas}>
+          
+          
+          <div>
+          <div className={styles.block__detail_precio}>Precio: ${p.price}</div>
+            Category:{p.category?.name}
+            
+            {p.brand && <p>Marca: {p.brand}</p>}
+            Condition:{p.condition}
+            {p.ram && <p>ram: {p.ram} GB</p>}
+            {p.processor && <p>procesador: {p.processor}</p>}
+            {p.battery && <p>uso continuo: {p.battery} horas</p>}
+            {p.bluetooth && <p>bluetooth</p>}
+            Description:{p.description}
+            
+          </div>
+          
+          <button className={styles.green_btn} onClick={addToCar}>Add to my Car</button>
+          
+        </div>
+
+        <div>
+        
+
+        </div>
+
+        
+      </div>
+    </div>
+        
+      </div>
+
+    </div>
+    </div>
+  );
+}*/
 
 
