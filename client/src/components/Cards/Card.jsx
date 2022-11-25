f27eb409e94f4b1db02a42495b4309aa1999bfe2
JulianLIMe <linero.julian.andres@gmail.com>
2022-11-25 18:49:11 -0500

    boton de agregar en cada card, mensaje si no esta logueado

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styles from './Card.module.css'
 import car from '../../assets/carrito2.png'
+import { shopingCar } from '../../store/actions/index.js'
+
 const Card = (props) => {
 
+  const dispatch = useDispatch()
 
+  function addToCar() {
+    console.log("In SHOPPING CAR");
+    dispatch(shopingCar("ADD", props.allProduct));
+  }
 
   return (
     <div className={styles.block__card}>
@@ -21,11 +29,9 @@ const Card = (props) => {
           <div className={styles.block__card_precio}>${props.price}</div>
         </div>
         <div className={styles.block__description}>
-          <Link to="/shoping">
-            <p>
-              <img className={styles.icon_car} src={car} />{' '}
-            </p>
-          </Link>
+            <button className={styles.icon_car} type='button' onClick={addToCar}>
+              <img src="https://w7.pngwing.com/pngs/380/261/png-transparent-computer-icons-shopping-cart-add-to-cart-button-text-logo-monochrome-thumbnail.png" alt="add shopping" width="30px" height="30px" />{' '}
+            </button>
         </div>
       </div>
     </div>
--- client/src/pages/ShopingCard/ShopingCard.jsx
+++ client/src/pages/ShopingCard/ShopingCard.jsx
@@ -14,7 +14,7 @@ const ShopingCard = () => {
 
   function reset() {
     let resp = window.confirm("¿Esta seguro de querer vaciar el carrito?")
-    if (resp === true){
+    if (resp === true) {
       dispatch(shopingCar("REST"));
       setSuiche(false)
     }
@@ -22,9 +22,11 @@ const ShopingCard = () => {
 
   console.log("prices", JSON.parse(sessionStorage.getItem("prices")))
 
-  const p = JSON.parse(sessionStorage.getItem("prices"))
-  const subTot = p.reduce((a, b) => a + b, 0)
-  console.log("pagar:", subTot);
+  if (sessionStorage.getItem("prices") !== null) {
+    const p = JSON.parse(sessionStorage.getItem("prices"))
+    var subTot = p.reduce((a, b) => a + b, 0)
+    console.log("pagar:", subTot);
+  }
 
   const [suiche, setSuiche] = useState(false);
   function pago() {
@@ -38,52 +40,52 @@ const ShopingCard = () => {
         <div className={styles.block__shopingcontenedor}>
           <div className={styles.block__shopingcontenedoruno}>
             <h3>Carrito de compras</h3>
-            
+
           </div>
           <div className={styles.block__profile_contendordosdiv}  ></div>
 
           <div className={styles.block__shopingcontenedordos}>
             {carrito.length === 0 && <p>Sin Productos</p>}
-              {carrito &&
-                carrito.map((e, i) => {
-                  return  (
-                    <div className={styles.block__shopingcontendortres} key={i}>
-                      
-                      <Card
-                        index={i}
-                        id={e._id}
-                        title={e.title}
-                        image={e.image}
-                        price={e.price}
-                        stock={e.stock}
-                      />
-                      
-                    </div>
-                  );
-                })}
+            {carrito.length > 0 &&
+              carrito.map((e, i) => {
+                return (
+                  <div className={styles.block__shopingcontendortres} key={i}>
 
-              
-              
-              
-              
-              
-              {suiche && <Payment price={subTot} />}
+                    <Card
+                      index={i}
+                      id={e._id}
+                      title={e.title}
+                      image={e.image}
+                      price={e.price}
+                      stock={e.stock}
+                    />
+
+                  </div>
+                );
+              })}
+
+
+
+
+
+
+            {suiche && <Payment price={subTot} />}
 
           </div>
           <div className={styles.block__profile_contendordosdiv}  ></div>
-          
+
 
           <div className={styles.block__shopingcontenedorseis}>
             {carrito.length > 0 && <button onClick={reset}>Reset carrito</button>}
-            <button  onClick={pago}>PAGAR</button>
+            <button onClick={pago}>PAGAR</button>
 
           </div>
-          
+
         </div>
-        
-          
+
+
       </div>
-      
+
     </div>
   );
 };
