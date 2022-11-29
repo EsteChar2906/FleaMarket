3817a049cb0866151caaf955bcc032d613d165ee
JulianLIMe <linero.julian.andres@gmail.com>
2022-11-29 10:28:56 -0500

    ajustando el profile de auth0

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -21,14 +21,19 @@ const Profile = () => {
           <div className={styles.block__profile_contendoruno}> 
             <img className={styles.block__profile_contendoruno_person}src={person} alt="" />          
             {userStorage && <h1>Username: {userStorage.username}</h1>}
+            {user && <h1>Username: {user.nickname}</h1>}
           </div>
 
           <div className={styles.block__profile_contendordos}>
-            <JSONPretty data={user} />
+            {/* <JSONPretty data={user} /> */}
             
             {userStorage && <h1>Name: {userStorage.name}</h1>}
+            {user && <h1>Name: {user.given_name}</h1>}
+            <div className={styles.block__profile_contendordosdiv}  ></div>
+            {user && <h1>Lastname: {user.family_name}</h1>}
             <div className={styles.block__profile_contendordosdiv}  ></div>
             {userStorage && <h1>Email: {userStorage.email}</h1>}
+            {user && <h1>Email: {user.email}</h1>}
             <div className={styles.block__profile_contendordosdiv}  ></div>
             {userStorage && <h1>Country: {userStorage.country}</h1>}
             <div className={styles.block__profile_contendordosdiv}  ></div>
