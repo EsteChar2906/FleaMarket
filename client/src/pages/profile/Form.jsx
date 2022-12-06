1e58082ecd7f6bccd2cd4fdb00f7309a7c7060c8
MilagrosVerde <milagrosverde1234@gmail.com>
2022-12-06 16:41:56 -0300

    arreglandoperfil

--- client/src/pages/profile/Form.jsx
+++ client/src/pages/profile/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import AuthContext from "../../context/authProvider";
 import { BACK_DOMINIO, FRONT_DOMINIO } from "../../config.js"
+import styles from "./Form.module.css"
 
 
 const Form = ({closeModal}) => {
@@ -46,39 +47,88 @@ const Form = ({closeModal}) => {
     }
 
     return (
-        <div>
-            <form >
-                <label>firstname </label>
-                <input type="text" placeholder={user.firstname} name="firstname" onChange={handleChanges} />
-                <br />
-                <label>lastname </label>
-                <input type="text" placeholder={user.lastname} name="lastname" onChange={handleChanges} />
-                <br />
-                <label>username </label>
-                <input type="text" placeholder={user.username} name="username" onChange={handleChanges} />
-                <br />
-                <label>email</label>
-                <input type="text" placeholder={user.email} name="email" onChange={handleChanges} />
-                <br />
-                {/* <label>password</label>
-                <input type="text" placeholder="new password" name="password" onChage={handleChanges} />
-                <br /> */}
-                <label>country</label>
-                <input type="text" placeholder={user.country} name="country" onChange={handleChanges} />
-                <br />
-                <label>city</label>
-                <input type="text" placeholder={user.city} name="city" onChange={handleChanges} />
-                <br />
-                <label>addres</label>
-                <input type="text" placeholder={user.addres} name="addres" onChange={handleChanges} />
-                <br />
-                <label>telephone</label>
-                <input type="text" placeholder={user.telephone} name="telephone" onChange={handleChanges} />
-                <br />
-            </form>
+        <div className={styles.block__contenedorformprofile}>
+            <div className={styles.block__contenedorform}>
+                <form className={styles.block__contenedorformdos} >
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Firstname</label>
+                        </div>
 
-            <button onClick={update}>Update</button>
+                        <input type="text" placeholder={user.firstname} name="firstname" onChange={handleChanges} />
+                        <br />
+                    </div>
 
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Lastname</label>
+                        </div>
+
+                        <input type="text" placeholder={user.lastname} name="lastname" onChange={handleChanges} />
+                        <br />
+                    </div>
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Username</label>
+                        </div>
+                        <input type="text" placeholder={user.username} name="username" onChange={handleChanges} />
+                        <br />
+                    </div>
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Email</label>
+                        </div>
+
+                        <input type="text" placeholder={user.email} name="email" onChange={handleChanges} />
+                        <br />
+                    </div>
+
+                    {/* <label>password</label>
+                    <input type="text" placeholder="new password" name="password" onChage={handleChanges} />
+                    <br /> */}
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Country</label>
+                        </div>
+
+                        <input type="text" placeholder={user.country} name="country" onChange={handleChanges} />
+                        <br />
+
+                    </div>
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>City</label>
+                        </div>
+
+                        <input type="text" placeholder={user.city} name="city" onChange={handleChanges} />
+                        <br />
+                    </div>                
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Addres</label>
+                        </div>
+
+                        <input type="text" placeholder={user.addres} name="addres" onChange={handleChanges} />
+                        <br />
+                    </div>
+
+                    <div className={styles.block__contenedordiv}>
+                        <div className={styles.block__contenedordivdos}>
+                            <label>Telephone</label>
+                        </div>
+
+                        <input type="text" placeholder={user.telephone} name="telephone" onChange={handleChanges} />
+                        <br />
+                    </div>
+
+                </form>        
+            </div>
+                <button className={styles.block__profilebutton} onClick={update}>Update</button>
         </div>
     )
 }
